b8f8a33d3c527df86c44053cb1be71d38b2ec85d
Eazzyzzo <israeloguntolu@gmail.com>
2022-10-17 14:58:17 -0700

    added format changes

--- utils.c
+++ utils.c
@@ -102,13 +102,9 @@ long int convert_size_number(long int num, int size)
 
 		return ((short)num);
 
-
-
 	return ((int)num);
 
 }
-
-
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
@@ -128,8 +124,6 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 	else if (size == S_SHORT)
 
 		return ((unsigned short)num);
-
-
 
 	return ((unsigned int)num);
 
--- write_handlers.c
+++ write_handlers.c
@@ -182,13 +182,13 @@ int write_unsgnd(int is_negative, int ind,
  * write_pointer - Write a memory address
  * @buffer: Arrays of chars
  * @ind: Index at which the number starts in the buffer
- * @length: Length of number
+ * @length: Length of the number
  * @width: Wwidth specifier
  * @flags: Flags specifier
  * @padd: Char representing the padding
  * @extra_c: Char representing extra char
  * @padd_start: Index at which padding should start
- * Return: Number of written chars.
+ * Return: Number of the written chars.
  */
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start)
